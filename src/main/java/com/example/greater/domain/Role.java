b0f73f80a17be4faaf91b58dfe40ebd493b5c44a
Staneone <staneone1@gmail.com>
2019-10-30 14:38:29 +0300

    4stepsBeforeFreemarker

---
+++ src/main/java/com/example/greater/domain/Role.java
@@ -0,0 +1,12 @@
+package com.example.greater.domain;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum Role implements GrantedAuthority {
+    USER;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
---
+++ src/main/java/com/example/greater/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.greater.repos;
+
+import com.example.greater.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
